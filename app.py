089e57dafa07142f6289b0b238688c1a668aeae7
UnQJack <60404877+UnQJack@users.noreply.github.com>
2023-12-10 23:54:51 +0200

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, send_file
 
 app = Flask(__name__)
 
-df=pd.read_csv('data.csv')
+df = pd.read_csv('data.csv')
 
 @app.route('/plot1')
 def plot1():
@@ -17,9 +17,9 @@ def plot1():
     plt.title('Toate valorile')
     plt.legend()
     buf = BytesIO()
-    fig.savefig(buf, format='png')
+    fig.savefig(buf,format='png')
     buf.seek(0)
-    return send_file(buf, mimetype='image/png')
+    return send_file(buf,mimetype='image/png')
 
 @app.route('/plot2')
 def plot2():
@@ -29,22 +29,22 @@ def plot2():
     plt.title('Primele 10 Valori')
     plt.legend()
     buf = BytesIO()
-    fig.savefig(buf, format='png')
+    fig.savefig(buf,format='png')
     buf.seek(0)
-    return send_file(buf, mimetype='image/png')
+    return send_file(buf,mimetype='image/png')
 
 @app.route('/plot3')
 def plot3():
-    ult=df[['Durata','Puls']].tail(15)
+    ult = df[['Durata','Puls']].tail(15)
     fig = plt.figure(figsize=(10,10))
     plt.plot(ult['Durata'],marker='x',label='Durata')
     plt.plot(ult['Puls'],marker='x',label='Puls')
     plt.title('Ultimele 15 Valori')
     plt.legend()
     buf = BytesIO()
-    fig.savefig(buf, format='png')
+    fig.savefig(buf,format='png')
     buf.seek(0)
-    return send_file(buf, mimetype='image/png')
+    return send_file(buf,mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)

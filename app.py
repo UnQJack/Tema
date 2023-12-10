2f937a149e3f104d9ffd5ebdde307cb333cbc05b
UnQJack <60404877+UnQJack@users.noreply.github.com>
2023-12-10 22:17:27 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+df=pd.read_csv('data.csv')
+
+@app.route('/plot1')
+def plot1():
+    fig = plt.figure(figsize=(10,10))
+    plt.plot(df.index,df['Durata'],label='Durata',marker='x')
+    plt.plot(df.index,df['Puls'],label='Puls',marker='x')
+    plt.plot(df.index,df['MaxPuls'],label='MaxPuls',marker='x')
+    plt.plot(df.index,df['Calorii'],label='Calorii',marker='x')
+    plt.title('Toate valorile')
+    plt.legend()
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png')
+
+@app.route('/plot2')
+def plot2():
+    fig = plt.figure(figsize=(10,10))
+    plt.plot(df['Durata'][:10],marker='x',label='Durata')
+    plt.plot(df['Puls'][:10],marker='x',label='Puls')
+    plt.title('Primele 10 Valori')
+    plt.legend()
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png')
+
+@app.route('/plot3')
+def plot3():
+    ult=df[['Durata','Puls']].tail(15)
+    fig = plt.figure(figsize=(10,10))
+    plt.plot(ult['Durata'],marker='x',label='Durata')
+    plt.plot(ult['Puls'],marker='x',label='Puls')
+    plt.title('Ultimele 15 Valori')
+    plt.legend()
+    buf = BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5001)
--- ex2.py
+++ ex2.py
@@ -1,28 +1,70 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from tkinter import Tk, Frame, Canvas, PhotoImage, Button
+from PIL import Image, ImageTk
 
-df=pd.read_csv('data.csv')
+class VisualizationApp:
+    def __init__(self, master, data):
+        self.master = master
+        self.master.title("Vizualizare Date")
+        self.data = data
 
-plt.figure(figsize=(10,10))
-plt.plot(df.index,df['Durata'],label='Durata',marker='x')
-plt.plot(df.index,df['Puls'],label='Puls',marker='x')
-plt.plot(df.index,df['MaxPuls'],label='MaxPuls',marker='x')
-plt.plot(df.index,df['Calorii'],label='Calorii',marker='x')
-plt.title('Toate valorile')
-plt.legend()
+        self.frame = Frame(self.master)
+        self.frame.pack()
 
-plt.figure(figsize=(10,10))
-plt.plot(df['Durata'][:10],marker='x',label='Durata')
-plt.plot(df['Puls'][:10],marker='x',label='Puls')
-plt.title('Primele 10 Valori')
-plt.legend()
+        self.canvas = Canvas(self.frame,width=800,height=600)
+        self.canvas.pack()
 
-ult=df[['Durata','Puls']].tail(15)
-plt.figure(figsize=(10,10))
-plt.plot(ult['Durata'],marker='x',label='Durata')
-plt.plot(ult['Puls'],marker='x',label='Puls')
-plt.title('Ultimele 15 Valori')
-plt.legend()
+        self.plot_all_button = Button(self.master,text="Toate Valorile",command=self.plot_all)
+        self.plot_all_button.pack()
 
-plt.tight_layout()
-plt.show()
+        self.plot_first_10_button = Button(self.master,text="Primele 10 Valori",command=self.plot_first_10)
+        self.plot_first_10_button.pack()
+
+        self.plot_last_15_button = Button(self.master, text="Ultimele 15 Valori",command=self.plot_last_15)
+        self.plot_last_15_button.pack()
+
+    def plot_all(self):
+        plt.figure(figsize=(8,6))
+        plt.plot(self.data.index,self.data['Durata'],label='Durata',marker='x')
+        plt.plot(self.data.index,self.data['Puls'],label='Puls',marker='x')
+        plt.plot(self.data.index,self.data['MaxPuls'],label='MaxPuls', marker='x')
+        plt.plot(self.data.index,self.data['Calorii'],label='Calorii', marker='x')
+        plt.title('Toate valorile')
+        plt.legend()
+
+        self.display_plot()
+
+    def plot_first_10(self):
+        plt.figure(figsize=(8,6))
+        plt.plot(self.data['Durata'][:10],marker='x',label='Durata')
+        plt.plot(self.data['Puls'][:10],marker='x',label='Puls')
+        plt.title('Primele 10 Valori')
+        plt.legend()
+
+        self.display_plot()
+
+    def plot_last_15(self):
+        ult = self.data[['Durata','Puls']].tail(15)
+        plt.figure(figsize=(8,6))
+        plt.plot(ult['Durata'],marker='x',label='Durata')
+        plt.plot(ult['Puls'],marker='x',label='Puls')
+        plt.title('Ultimele 15 Valori')
+        plt.legend()
+
+        self.display_plot()
+
+    def display_plot(self):
+        plt.tight_layout()
+        plt.savefig("plot.png")
+        image = Image.open("plot.png")
+        photo = ImageTk.PhotoImage(image)
+        self.canvas.create_image(0,0,anchor='nw',image=photo)
+        self.canvas.image = photo
+
+if __name__ == "__main__":
+    df = pd.read_csv('data.csv')
+
+    root = Tk()
+    app = VisualizationApp(root, df)
+    root.mainloop()
